13c1992ba47629d351fd7f29edc2e5e4aa701299
santiago <santiagocasamayor14@gmail.com>
2023-07-24 17:16:48 -0400

    aaa

---
+++ comunes/header.php
@@ -0,0 +1,34 @@
+
+	
+
+
+    
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+
+      <title>U.E.COLEGIO - Carlos Soublette</title>
+	  
+      
+      
+      <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	    <!----css3---->
+        <link rel="stylesheet" href="assets/css/custom.css">
+		
+		
+		<!--google fonts -->
+	    <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+	
+	
+	   <!--google material icon-->
+        <link href="https://fonts.googleapis.com/css2?family=Material+Icons"rel="stylesheet">
+
+    
+        <link href="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-1.13.5/b-2.4.1/b-html5-2.4.1/r-2.5.0/datatables.min.css" rel="stylesheet">
+ 
+
+        <script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-1.13.5/b-2.4.1/b-html5-2.4.1/r-2.5.0/datatables.min.js"></script>
